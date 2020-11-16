61699fd714142becaeea7adcbd7fe0a9bc37e6b8
Timothy Samraj <tsam0005@student.monash.edu>
2020-11-16 15:32:35 +1100

    Reformat files

--- src/Sys.hs
+++ src/Sys.hs
@@ -1,27 +1,30 @@
- {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Sys (setSuspendState, lockWorkStation) where
 
-import System.Win32.DLL ( getProcAddress, loadLibraryEx )
-import System.Win32.Types ( nullHANDLE, Addr )
-import Foreign.Ptr ( FunPtr, castPtrToFunPtr )
+import Foreign.Ptr (FunPtr, castPtrToFunPtr)
+import System.Win32.DLL (getProcAddress, loadLibraryEx)
+import System.Win32.Types (Addr, nullHANDLE)
 
 type TripleBool = Bool -> Bool -> Bool -> IO ()
+
 foreign import ccall "dynamic" setSuspendState0 :: FunPtr TripleBool -> TripleBool
+
 foreign import ccall "dynamic" lockWorkStation0 :: FunPtr (IO ()) -> IO ()
 
 getFuncAddr :: String -> String -> IO Addr
 getFuncAddr dllName methodName = do
-    dll <- loadLibraryEx dllName nullHANDLE 0x00000800
-    getProcAddress dll methodName
+  dll <- loadLibraryEx dllName nullHANDLE 0x00000800
+  getProcAddress dll methodName
 
 -- true: hibernate
 -- false: sleep
 setSuspendState :: Bool -> IO ()
 setSuspendState b1 = do
-    addr <- getFuncAddr "PowrProf.dll" "SetSuspendState"
-    setSuspendState0 (castPtrToFunPtr addr) b1 False False
+  addr <- getFuncAddr "PowrProf.dll" "SetSuspendState"
+  setSuspendState0 (castPtrToFunPtr addr) b1 False False
 
 lockWorkStation :: IO ()
 lockWorkStation = do
-    addr <- getFuncAddr "User32.dll" "LockWorkStation"
-    lockWorkStation0 $ castPtrToFunPtr addr
+  addr <- getFuncAddr "User32.dll" "LockWorkStation"
+  lockWorkStation0 $ castPtrToFunPtr addr
