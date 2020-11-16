8cd8a97d13674580c83c2de492cf7c39e71e0f48
Timothy Samraj <tsam0005@student.monash.edu>
2020-11-16 16:51:46 +1100

    Fix up main

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,4 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-
-import Foreign.Ptr (FunPtr, castPtrToFunPtr)
-import Lib
-import System.Win32.DLL (getProcAddress, loadLibraryEx)
-import System.Win32.Types (Addr, nullHANDLE)
-
-type TripleBool = Bool -> Bool -> Bool -> IO ()
-
-foreign import ccall "dynamic" setSuspendState0 :: FunPtr TripleBool -> TripleBool
-
-foreign import ccall "dynamic" lockWorkStation0 :: FunPtr (IO ()) -> IO ()
-
-getFuncAddr :: String -> String -> IO Addr
-getFuncAddr dllName methodName = do
-  dll <- loadLibraryEx dllName nullHANDLE 0x00000800
-  getProcAddress dll methodName
-
--- true: hibernate
--- false: sleep
-setSuspendState :: Bool -> IO ()
-setSuspendState b1 = do
-  addr <- getFuncAddr "PowrProf.dll" "SetSuspendState"
-  setSuspendState0 (castPtrToFunPtr addr) b1 False False
-
-lockWorkStation :: IO ()
-lockWorkStation = do
-  addr <- getFuncAddr "User32.dll" "LockWorkStation"
-  lockWorkStation0 $ castPtrToFunPtr addr
+import Web (mainLoop)
 
 main :: IO ()
--- main = setSuspendState True
-main = lockWorkStation
-
--- main = mainLoop
+main = mainLoop
