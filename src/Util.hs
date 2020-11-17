d727b29097755aa1ed61fbc49fb426570ae5289b
Timothy Samraj <tsam0005@student.monash.edu>
2020-11-17 21:20:29 +1100

    Add logging functions

--- src/Util.hs
+++ src/Util.hs
@@ -1,9 +1,24 @@
 module Util where
 
 import Data.Aeson.Types (FromJSON (parseJSON), Value, parseMaybe)
+import Data.Time (defaultTimeLocale, formatTime, getZonedTime)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import qualified Sys as SYS
 import System.Environment (getEnv)
+import System.IO (IOMode (AppendMode), hClose, hPutStrLn, openFile)
+
+logTimeStr :: IO String
+logTimeStr = do
+  now <- getZonedTime
+  let str = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" now
+  pure $ "[" <> str <> "] "
+
+writeLog :: String -> IO ()
+writeLog str = do
+  file <- openFile ".\\run.log" AppendMode
+  time <- logTimeStr
+  hPutStrLn file $ time <> str
+  hClose file
 
 currentUnixTime :: IO Integer
 currentUnixTime = floor <$> getPOSIXTime
